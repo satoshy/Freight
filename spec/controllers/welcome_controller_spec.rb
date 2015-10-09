ff68070e1d27dc0819a3f41b496f43e243de20c3
satoshy <alex.stipanenko@gmail.com>
2015-10-10 01:23:52 +0300

    add welcome controller

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do
+  describe "GET index" do
+    it "has a 200 status code" do
+      get :index 
+      expect(response.status).to eq(200)
+    end
+    
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+end
